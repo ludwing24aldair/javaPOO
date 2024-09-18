1f11942ba9c046c46c5d60b2f831f0000207bcc7
ludwing24aldair <1480905@senati.pe>
2024-09-18 11:25:52 -0500

    Agregando nombre

--- src/vehiculo/Principal.java
+++ src/vehiculo/Principal.java
@@ -1,5 +1,5 @@
 package vehiculo;
-
+//Autor: LUDWING ALDAIR MAMANI YUCRA
 public class Principal {
     public static void main(String[] args) {
         //instanciar = invocar = llamar a un objeto
